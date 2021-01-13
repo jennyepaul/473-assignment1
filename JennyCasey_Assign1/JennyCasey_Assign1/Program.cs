fd38f6cbd5b8e1c23e577e0f5c37ad481b215d57
Jenny Paul <jenny@LAPTOP-PQ2SB0U6>
2021-01-13 13:59:37 -0600

    adding in basics of file (see if this works)

---
+++ JennyCasey_Assign1/JennyCasey_Assign1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JennyCasey_Assign1
+{
+
+    class Program
+    {
+        public enum ItemType
+        {
+            Helmet, Neck, Shoulders, Back, Chest,
+            Wrist, Gloves, Belt, Pants, Boots,
+            Ring, Trinket
+        };
+
+        public enum Race { Orc, Troll, Tauren, Forsaken };
+
+        private static uint MAX_ILVL = 360;
+        private static uint MAX_PRIMARY = 200;
+        private static uint MAX_STAMINA = 275;
+        private static uint MAX_LEVEL = 60;
+        private static uint GEAR_SLOTS = 14;
+        private static uint MAX_INVENTORY_SIZE = 20;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
