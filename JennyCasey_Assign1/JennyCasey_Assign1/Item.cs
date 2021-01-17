9112230014cd0dfdd6f89bab025db39ccd392f4a
Jenny Paul <jenny@LAPTOP-PQ2SB0U6>
2021-01-17 16:39:44 -0600

    added in start of Item class, and some changes to main method

---
+++ JennyCasey_Assign1/JennyCasey_Assign1/Item.cs
@@ -0,0 +1,165 @@
+﻿/* CSCI473 
+ * Assignment 1
+ * TEAM: JennyCasey
+ * Contributors: Jennifer Paul (z1878099) and Casey McDermott (ID HERE)
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JennyCasey_Assign1
+{
+    
+    class Item
+    {
+        //constant to be used
+        private static uint MAX_ILVL = 360;
+        private static uint MAX_PRIMARY = 200;
+        private static uint MAX_STAMINA = 275;
+        private static uint MAX_LEVEL = 60;
+
+        //private attributes of Item class
+        private readonly uint _id;
+        private string name;
+        private ItemType type;
+        private uint ilvl;
+        private uint primary;
+        private uint stamina;
+        private uint requirement;
+        private string flavor;
+
+        //default constructor
+        public Item()
+        {
+            _id = 0;
+            name = "";
+            type = 0;
+            ilvl = 0;
+            primary = 0;
+            stamina = 0;
+            requirement = 0;
+            flavor = "";   
+        }
+
+        //overloaded constructor to assign initial values to all attributes
+        public Item(uint _id, string name, ItemType type, uint ilvl, uint primary, uint stamina, uint requirement, string flavor)
+        {
+            this._id = _id;
+            this.name = name;
+            this.type = type;
+            this.ilvl = ilvl;
+            this.primary = primary;
+            this.stamina = stamina;
+            this.requirement = requirement;
+            this.flavor = flavor;
+        }
+        public uint ID
+        {
+            //only a getter, since only readonly
+            get
+            {
+                return _id;
+            }
+        }
+
+        public string Name
+        {
+            //free read/write access so both a setter and getter
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public ItemType Type
+        {
+            //free read/write access so both getter and setter
+            get
+            {
+                //might need subscript here to get specific type?
+                return type;
+            }
+            set
+            {
+                /*
+                if(value < 0 || value > 12)
+                {
+                    throw new Exception("Valid range is [0,12]");
+                }
+                */
+               
+                type = value;
+            }
+        }
+
+        public uint Ilvl
+        {
+            //free read/write access so getter and setters
+            get
+            {
+                return ilvl;
+            }
+            set
+            {
+                //put a check in here for valid range
+                ilvl = value;
+            }
+        }
+
+        public uint Primary
+        {
+            //read and write access so getter and setter
+            get
+            {
+                return primary;
+            }
+            set
+            {
+                primary = value;
+            }
+        }
+
+        public uint Stamina
+        {
+            //read and write access so getter and setter
+            get
+            {
+                return stamina;
+            }
+            set
+            {
+                stamina = value;
+            }
+        }
+
+        public uint Requirement
+        {
+            //read and write access, so getter and setter
+            get
+            {
+                return requirement;
+            }
+            set
+            {
+                requirement = value;
+            }
+        }
+
+        public string Flavor
+        {
+            //read and write access, so getter and setter
+            get
+            {
+                return flavor;
+            }
+            set
+            {
+                flavor = value;
+            }
+        }
+    }
+}
--- JennyCasey_Assign1/JennyCasey_Assign1/Program.cs
+++ JennyCasey_Assign1/JennyCasey_Assign1/Program.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿/* CSCI473 
+ * Assignment 1
+ * TEAM: JennyCasey
+ * Contributors: Jennifer Paul (z1878099) and Casey McDermott (ID HERE)
+ */
+using System;
 using System.Text;
 
 namespace JennyCasey_Assign1
 {
-
+    public enum ItemType
+    {
+        Helmet, Neck, Shoulders, Back, Chest,
+        Wrist, Gloves, Belt, Pants, Boots,
+        Ring, Trinket
+    };
     class Program
     {
         public enum ItemType
@@ -12,13 +22,14 @@ namespace JennyCasey_Assign1
             Wrist, Gloves, Belt, Pants, Boots,
             Ring, Trinket
         };
-
         public enum Race { Orc, Troll, Tauren, Forsaken };
 
+        /*
         private static uint MAX_ILVL = 360;
         private static uint MAX_PRIMARY = 200;
         private static uint MAX_STAMINA = 275;
         private static uint MAX_LEVEL = 60;
+        */
         private static uint GEAR_SLOTS = 14;
         private static uint MAX_INVENTORY_SIZE = 20;
         private const string formatMenuString = "\t{0}\n\t{1}\n\t{2}\n\t{3}\n\t{4}\n\t{5}\n\t{6}\n\t{7}\n\t{8}\n\t{9}";
@@ -35,59 +46,62 @@ namespace JennyCasey_Assign1
             string choiceNine = "9.) Award Experience";
             string quit = "10.) Quit";
             string choice;
+            bool isContinuing = true;
 
             //print out the menu and the options to the user
             Console.WriteLine("Welcome to the World of ConflictCraft: Testing Environment!");
-            Console.WriteLine("Welcome to World of ConflictCraft: Testing Environment. Please select an option from the list below:");
-            Console.WriteLine(String.Format(formatMenuString,choiceOne,choiceTwo,choiceThree,choiceFour,
-                                                            choiceFive,choiceSix,choiceSeven,choiceEight,
-                                                            choiceNine,quit));
-     
-            //read the choice from the user
-            choice = Console.ReadLine();
-
-            //switch statement to do the various tasks based on what the user enters
-            //for now it just prints back what they chose, will add functionality to this
-            //as we build the classes
-            //might have to put this within a while loop so that the user can keep entering a choice
-            switch(choice)
+            
+            //while loop checks for flag if we should continue, this will be set to false (do not continue, exit out of program) when
+            //the user enters any of the 'quit' options
+            while(isContinuing == true)
             {
-                case "1":
-                    Console.WriteLine("You chose to print all the players out!");
-                    break;
-                case "2":
-                    Console.WriteLine("You chose to print all the guilds available!");
-                    break;
-                case "3":
-                    Console.WriteLine("You chose to list all the gear");
-                    break;
-                case "4":
-                    Console.WriteLine("You chose to print the gear list for the player ");
-                    break;
-                case "5":
-                    Console.WriteLine("You chose to leave the guild");
-                    break;
-                case "6":
-                    Console.WriteLine("You chose to join a guild!");
-                    break;
-                case "7":
-                    Console.WriteLine("You chose to equip some gear!");
-                    break;
-                case "8":
-                    Console.WriteLine("You chose to unequip some gear!");
-                    break;
-                case "9":
-                    Console.WriteLine("Let's award some experience now");
-                    break;
-                case "10": case "q": case "Q": case "quit": case "Quit": case "exit": case "Exit":
-                    Console.WriteLine("Exiting program...");
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice");
-                    break;
+                //beginning message and menu options
+                Console.WriteLine("Welcome to World of ConflictCraft: Testing Environment. Please select an option from the list below:");
+                Console.WriteLine(String.Format(formatMenuString, choiceOne, choiceTwo, choiceThree, choiceFour,
+                                                                choiceFive, choiceSix, choiceSeven, choiceEight,
+                                                                choiceNine, quit));
+                //read the choice from the user
+                choice = Console.ReadLine();
+
+                //switch to perform various tasks based on what option the user enters
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("You chose to print all the players out!");
+                        break;
+                    case "2":
+                        Console.WriteLine("You chose to print all the guilds available!");
+                        break;
+                    case "3":
+                        Console.WriteLine("You chose to list all the gear");
+                        break;
+                    case "4":
+                        Console.WriteLine("You chose to print the gear list for the player ");
+                        break;
+                    case "5":
+                        Console.WriteLine("You chose to leave the guild");
+                        break;
+                    case "6":
+                        Console.WriteLine("You chose to join a guild!");
+                        break;
+                    case "7":
+                        Console.WriteLine("You chose to equip some gear!");
+                        break;
+                    case "8":
+                        Console.WriteLine("You chose to unequip some gear!");
+                        break;
+                    case "9":
+                        Console.WriteLine("Let's award some experience now");
+                        break;
+                    case "10": case "q": case "Q": case "quit": case "Quit": case "exit": case "Exit":
+                        Console.WriteLine("Exiting program...");
+                        isContinuing = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
             }
-
-
         }
     }
 }
